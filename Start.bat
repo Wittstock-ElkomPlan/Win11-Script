b57563df2e6758d6d570d0efdf16b95b913e87df
Wittstock-ElkomPlan <72492916+Wittstock-ElkomPlan@users.noreply.github.com>
2024-08-26 10:48:30 +0200

    Download Version from Git

--- Start.bat
+++ Start.bat
@@ -1,2 +1,3 @@
+START /B /wait powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/Wittstock-ElkomPlan/Win11-Script/main/Win11-Script.ps1 -OutFile '%~dp0\Win11-Script.ps1'"
 PowerShell.exe -ExecutionPolicy Bypass -File "%~dp0\Win11-Script.ps1" 
 pause
